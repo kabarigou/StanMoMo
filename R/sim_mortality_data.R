c746b9518763e4a7e35c438acab3671e6bf5821a
kabarigou <u0105906@kuleuven.be>
2020-11-13 16:17:03 +0100

    Include function to simulate from a Renshaw-Haberman model

--- R/sim_mortality_data.R
+++ R/sim_mortality_data.R
@@ -47,6 +47,34 @@ sim_death_apc <- function(a, k, g, phi, years, ages, exposure){
   )
 }
 
+#' sim_death_rh simulate death counts from Renshaw-Haberman mortality model
+#'
+#' @param a vector of age component
+#' @param k vector of period component
+#' @param g vector of cohort component
+#' @param phi dispersion parameter
+#' @param years vector of calendar years
+#' @param ages vectors of ages
+#' @param exposure matrix of exposure data
+#'
+#' @return matrix of death count
+#' @export
+#'
+#' @examples
+sim_death_rh <- function(a, b,k, g, phi, years, ages, exposure){
+
+  cohorts <- sort(unique(as.vector(sapply(years, function(year) year - ages))))
+
+  gxt_apc <- 0 * exposure[,1:length(k)]
+  for(i in 1:length(a)){
+    for(j in 1:length(k)){
+      gxt_apc[i,j] <- exp(a[i] + b[i]*k[j] + g[match(years[j] - ages[i], cohorts)]) * exposure[i,j]
+    }
+  }
+  return(apply(gxt_apc, 1:2, function(gxt) rnbinom(1,size = phi, prob = phi / (phi + gxt)))
+  )
+}
+
 #' sim_mortality_data simulate mortality data from various models
 #'
 #' @param a vector of age component
@@ -68,6 +96,8 @@ sim_mortality_data <- function(a, k, b, g, phi, years, ages, exposure, mortality
     res <- sim_death_lc(a, b, k, phi, exposure)
   }else if(mortality_model == "apc"){
     res <- sim_death_apc(a, k, g, phi, years, ages, exposure)
+  }else if (mortality_model=="rh"){
+    res <- sim_death_rh(a, b,k, g, phi, years, ages, exposure)
   }
   return(res)
 }
