b3c4823777809c01b3a74410ee42579750cdba47
Karim Barigou <karim290492@gmail.com>
2021-07-24 12:46:33 +0200

    Correction of notes related to the forecasting_plot function

--- R/forecasting_plot.R
+++ R/forecasting_plot.R
@@ -1,6 +1,6 @@
 #' Fanplot for the mortality predictions
 #'
-#' The function generates a fan plot representing the forecast death rates based on the \code{ggplot2} and 
+#' The function generates a fan plot representing the forecast death rates based on the \code{ggplot2} and
 #' \code{ggfan} R packages.
 #'
 #' @param stan_fit stanfit object
@@ -20,31 +20,33 @@
 #' ages <- 50:90
 #' death <- load_HMD_data('BEL', 'Deaths_1x1', years, ages, "Male")$mat
 #' exposure <- load_HMD_data('BEL', 'Exposures_1x1', years, ages, "Male")$mat
-#' fitLC<-lc_stan(death = deathFR,exposure=exposureFR, forecast = 10, family = "poisson",chains=1,cores=1)
+#' fitLC<-lc_stan(death = deathFR,exposure=exposureFR, forecast = 10,
+#' family = "poisson",chains=1,cores=1)
 #' forecasting_plot(fitLC,ages,years,death,exposure,c(65,75,85))
 #' }
 #'
 forecasting_plot <- function(stan_fit, ages, years,death, exposure,ages.plot){
+  Age <- Obs <- Year <- NULL
   params<-rstan::extract(stan_fit)
   nforecast<-ncol(params$k_p)
   years.predict<-seq(years[length(years)]+1,years[length(years)]+nforecast,by=1)
-  
+
   pred<-array(params$mufor,dim=list(nrow(params$mufor),ncol(params$mufor)/nforecast,nforecast),
               dimnames = list(c(1:nrow(params$mufor)),formatC(ages),formatC(years.predict)))
-  
+
   pred<-as.data.frame.table(pred)
   colnames(pred)<-c("Sim","Age","Year","Obs")
   pred$Age <-as.numeric(as.character(pred$Age))
   pred$Year <-as.numeric(as.character(pred$Year))
-  
+
   qxt <- death / exposure
   qxt<-as.data.frame.table(qxt)
   colnames(qxt)<-c("Age","Year","Obs")
   qxt$Age <-as.numeric(as.character(qxt$Age))
   qxt$Year <-as.numeric(as.character(qxt$Year))
-  
+
   years<-c(years,years.predict)
-  
+
   ggplot2::ggplot()+
     ggplot2::geom_point(data=subset(qxt,Age %in% ages.plot & Year %in% years),ggplot2::aes(x=Year,y=Obs,shape=as.factor(Age)))+
     ggplot2::scale_y_log10()+
@@ -52,5 +54,5 @@ forecasting_plot <- function(stan_fit, ages, years,death, exposure,ages.plot){
     ggplot2::theme_bw()+
     ggplot2::scale_fill_distiller(palette="Spectral")+
     ggplot2::labs(y="death rate (log scale)", x="Year",shape="Age")
-  
+
 }
